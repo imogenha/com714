5e1e75e71bf0d5f368c0bd5d1ab923a72e259a68
imogenha <2harri05@solent.ac.uk>
2024-01-30 16:48:19 +0000

    Added appropriate docstrings documentation.

--- Practice Lab - Week 2/life/human.py
+++ Practice Lab - Week 2/life/human.py
@@ -7,22 +7,28 @@ class Human:
         self.energy = energy
 
     def __repr__(self) -> str:
+        """Print instance attributes within a string."""
         return f'human(name={self.name}, age={self.age}, energy={self.energy})'
 
     def __str__(self) -> str:
+        """Print a string which displays instance attributes within a sentence."""
         return f'{self.name} is {self.age} years old, and has {self.energy} energy'
 
     def grow(self) -> int:
+        """Increase age attribute by 1."""
         self.age += 1
         return self.age
 
     def eat(self, amount: int) -> int:
+        """Take an input energy consumption input, add it to current energy attribute. Return excess over max energy."""
         self.energy += amount
         excess = self.energy - Human.MAX_ENERGY
         print(f'{self.name} has {excess} excess energy')
         return excess
 
     def reproduce(self) -> bool:
+        """Assess current energy levels. Reproduce if over 20 and decrease energy, return true.
+        Else, return false and print appropriate message."""
         if self.energy >= 20:
             can_produce = True
             self.energy -= 20
@@ -33,6 +39,8 @@ class Human:
         return can_produce
 
     def move(self, distance: int) -> bool:
+        """Take a distance input, and times it by energy cost. If total energy cost is less than current energy levels,
+        perform a move action, decreasing energy, returning true. Else, return false and print appropriate message."""
         move_energy = 10
         total_energy = move_energy * distance
         if self.energy >= total_energy:
@@ -44,6 +52,4 @@ class Human:
             print(f'{self.name} has no energy to move!')
         return can_move
 
-
-h1 = Human("John", 28, 70)
 
