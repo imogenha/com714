57178f11ee3a838097302335391f123a8f054fb8
imogenha <2harri05@solent.ac.uk>
2024-02-01 15:17:31 +0000

    Q7 completed and tested for accuracy. Separate module given for reading user choices.

---
+++ Practice Lab - Week 1/Q7-Main.py
@@ -0,0 +1,6 @@
+import Q7
+
+phrase = input("Enter a Phrase: ")
+option = int(input("Enter a choice between 1-4: "))
+
+Q7.box_phrase(phrase, option)
--- Practice Lab - Week 1/Q7.py
+++ Practice Lab - Week 1/Q7.py
@@ -1,8 +1,6 @@
 # Prompt the user to enter a phrase
 
-def box_phrase():
-    phrase = input("Enter a Phrase: ")
-    option = int(input("Enter a choice between 1-4: "))
+def box_phrase(phrase, option):
     padding = len(phrase) + 1
     row = len(phrase) + 4
     h = ''.join(['+'] + ['-' * row] + ['+'])
@@ -20,16 +18,8 @@ def box_phrase():
         print(result)
 
     elif option == 4:
-        define_n = int(input("Enter a value: "))
-
-
-box_phrase()
-
-
-# Option 1: display a box with a phrase inside it.
-
-# Option 2: Display a box to the right of the phrase.
-
-# Option 3: Display a box to the left of the phrase.
-
-# Option 4: Display a box that is n x n.
+        n = int(input("Enter a value: "))
+        row = (len(phrase + " ") * n) + 4
+        h = ''.join(['+'] + ['-' * row] + ['+'])
+        result = h + (('\n'"|" + "  " + ((phrase + " ") * n) + "  " + "|"'\n') * n) + h
+        print(result)
