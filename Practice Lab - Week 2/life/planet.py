d54907b88cf2377dac6840e96c31e7cc5a56a7ba
imogenha <2harri05@solent.ac.uk>
2024-02-06 14:23:32 +0000

    Started code for Planet class.

---
+++ Practice Lab - Week 2/life/planet.py
@@ -0,0 +1,27 @@
+class Planet:
+    def __init__(self, name: str = ''):
+        self.__name = name
+        self.__humans = []
+
+    def __repr__(self):
+        return f'Planet(Name = {self.__name}, Humans = {self.__humans.__repr__()})'
+
+    def __str__(self):
+        return f'Planet {self.__name} has {self.__humans} humans.'
+
+    def add(self, human:Human) -> bool:
+        self.__humans.append(human)
+        return human in self.__humans
+
+    def remove(self, human:Human) -> bool:
+        self.__humans.remove(human)
+        return human in self.__humans
+
+    def has(self, human:Human) -> bool:
+        return human in self.__humans
+
+
+p1 = Planet(name='Orb')
+
+print(p1.add(human))
+print(p1.has(human))
